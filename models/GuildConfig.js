bf722ab74bc4c1e4af11d3450f8247eb4e254ff9
elimtinofarakawanza-png <elimtinofarakawanza@gmail.com>
2026-02-23 09:50:18 +0000

    Delete models/GuildConfig.js

--- models/GuildConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const guildConfigSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  reportChannel: { type: String, default: null }
-});
-
-module.exports = mongoose.model('GuildConfig', guildConfigSchema);
